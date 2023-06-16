463dfcb96c513236244bfa8736ab64b023219612
vikkstar123419 <aelfil8@gmail.com>
2023-06-16 09:20:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Smile in the mirror
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
