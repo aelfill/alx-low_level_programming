8a815b47a98e96679ac330ff911f25b6f6a7d663
vikkstar123419 <aelfil8@gmail.com>
2023-06-20 08:07:06 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - I sometimes suffer from insomnia
+ * Return:void
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 97; i <= 122; i++)
+	_putchar(i);
+	_putchar('\n');
+}
+
