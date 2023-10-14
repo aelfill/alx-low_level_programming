a2f14e4e8fa1bcd4c4f368914eaee2488a15c842
aelfill <aelfil8@gmail.com>
2023-10-14 09:30:29 -0700

    README.md

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - islower
+ * @c: the character to be checked
+ * Return: 1 for lowercase character. 0 for the rest
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+		return (1);
+	}
+		return (0);
+}
+
